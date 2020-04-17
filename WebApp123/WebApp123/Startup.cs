62b49d5ada7af40c2f45f232432c400f5308a5ad
baosql <boludahun@gmail.com>
2020-04-16 21:04:40 -0500

    latestt change
    Related Work Items: #1

--- WebApp123/WebApp123/Startup.cs
+++ WebApp123/WebApp123/Startup.cs
@@ -14,6 +14,7 @@ namespace WebApp123
     
     // Remove some codes 
     // doing a rerun
+    // tryin again
     public class Startup
     {
         public Startup(IConfiguration configuration)
